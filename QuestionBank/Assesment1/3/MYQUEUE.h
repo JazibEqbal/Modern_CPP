d1e226828537768ae7d24b6e175d81693e976882
JazibEqbal <jazibeqbal0526@gmail.com>
2023-06-01 22:51:47 +0530

    Q3

--- QuestionBank/Assesment1/3/MYQUEUE.h
+++ QuestionBank/Assesment1/3/MYQUEUE.h
@@ -11,20 +11,23 @@ private:
     int myQueueSize;                        
     int myQueueCapacity;
 public:
-    MYQUEUE(const MYQUEUE<T> &obj) = default;
     MYQUEUE() = delete;
+    MYQUEUE(const MYQUEUE<T> &obj) = default;
     //a constructor that takes reference to standard list of type T
-    MYQUEUE(std::list<T> &obj);
+    MYQUEUE(std::list<T> values);
     MYQUEUE(int capacity);
+    MYQUEUE(std::list<T> values,int size,int capacity);
+    ~MYQUEUE();
 
-    bool isEmpty(){
-        return (this->getMyQueueSize() == 0);
-    }
-    T pushBack(T arg);
-    T popFront(T value);
+    bool isEmpty();
+    void pushBack(T arg);
+    T popFront();
 
     T const& PeekFront();
     T const& PeekBack();
+
+    std::list<T> getMyQueueValues() const { return myQueueValues; }
+    void setMyQueueValues(const std::list<T> &myQueueValues_) { myQueueValues = myQueueValues_; }
 
     int getMyQueueSize() const { return myQueueSize; }
     void setMyQueueSize(int myQueueSize_) { myQueueSize = myQueueSize_; }
@@ -32,16 +35,14 @@ public:
     int getMyQueueCapacity() const { return myQueueCapacity; }
     void setMyQueueCapacity(int myQueueCapacity_) { myQueueCapacity = myQueueCapacity_; }
 
-    std::list<T> getMyQueueValues() const { return myQueueValues; }
-    void setMyQueueValues(const std::list<T> &myQueueValues_) { myQueueValues = myQueueValues_; }
 
     template <typename A>
     friend std::ostream &operator<<(std::ostream &os, const MYQUEUE<A> &rhs) {
     os << "myQueueSize: " << rhs.getMyQueueSize()<<"\n"
        << "myQueueCapacity: " << rhs.getMyQueueCapacity()<<"\n"
        << "myQueueValues: \n";
-       for(int i=0;i<3;i++){
-        os << rhs.myQueueValues[i]<<"\n";
+       for(auto it: rhs.myQueueValues){
+        os<<it;
     };
     return os;
 }
@@ -49,43 +50,60 @@ public:
 };
 
 template <typename T>
-inline MYQUEUE<T>::MYQUEUE(std::list<T> &elements)
-{
-    for(int i=0;i<elements.size();i++){
-        this->myQueueValues = elements;
-        this->myQueueSize++;
-    }
-}
+inline MYQUEUE<T>::MYQUEUE(std::list<T> elements) :myQueueValues{elements} {}
 //creates an empty queue and sets myQueueCapacity to capacity parameter
 template <typename T>
 inline MYQUEUE<T>::MYQUEUE(int capacity)
 {
-    MYQUEUE<T> *t;
-    t->myQueueCapacity=capacity;
+    MYQUEUE<T> *q;
+    q->myQueueCapacity=capacity;
 }
 
 template <typename T>
-inline T MYQUEUE<T>::pushBack(T arg)
+inline MYQUEUE<T>::MYQUEUE(std::list<T> values, int size, int capacity)
+: myQueueValues{values}, myQueueSize{size}, myQueueCapacity{capacity}{}
+
+template <typename T>
+inline MYQUEUE<T>::~MYQUEUE()
 {
-    return *(this->myQueueValues.end() + myQueueCapacity++) = arg;  //pushing one element to queue at the end of the list 
+    std::cout<<"Destructor\n";
 }
 
 template <typename T>
-inline T MYQUEUE<T>::popFront(T value)
+inline bool MYQUEUE<T>::isEmpty()
 {
-    return value = *(this->myQueueValues.begin()--);  //popping out first element
+    return myQueueValues.empty();
+}
+
+template <typename T>
+inline void MYQUEUE<T>::pushBack(T arg)
+{
+    return myQueueValues.push_back(arg);
+    //return *(this->myQueueValues.end() + myQueueCapacity++) = arg;  //pushing one element to queue at the end of the list 
+}
+
+template <typename T>
+inline T MYQUEUE<T>::popFront()
+{
+    if(myQueueValues.empty()){
+        throw std::runtime_error("Queue is empty");
+    }
+    return myQueueValues.pop_front();
+    //return *(this->myQueueValues.begin()--);  //popping out first element
 }
 
 template <typename T>
 inline T const &MYQUEUE<T>::PeekFront()
 {
-    return *(this->myQueueValues.begin());      //checking what is the first element in queue
+    return myQueueValues.front();
+    //return *(this->myQueueValues.begin());      //checking what is the first element in queue
 }
 
 template <typename T>
 inline T const &MYQUEUE<T>::PeekBack()
 {
-    return *(this->myQueueValues.end());        //checking what is the last element of queue
+    return myQueueValues.back();
+    //return *(this->myQueueValues.end());        //checking what is the last element of queue
 }
 
 
--- QuestionBank/Assesment1/3/functionalities.h
+++ QuestionBank/Assesment1/3/functionalities.h
@@ -5,40 +5,38 @@ int FindValueAtGivenPosition(MYQUEUE<T> *val, int position){
     if(position >= val->getMyQueueSize() || position < 0){   //checking if position is valid or not
         throw std::runtime_error("Invalid");
     }
-    if(val->getMyQueueSize() <= 0){                          //checking if queue is empty or not
-        throw std::runtime_error("Queue is empty");
+    if(val->getMyQueueSize() == 0){                          //checking if queue is empty or not
+        throw std::invalid_argument("Queue is empty");
     }
     return (val->getMyQueueValues().front() + position);       //going to the first elemnt and adding position to it and returning the value where pointer points now
 }
 
 template<typename T>
-MYQUEUE<T> MakeCombinedQueue(MYQUEUE<T> *t1, MYQUEUE<T> *t2){
-    MYQUEUE<T> *t3;                                                //creating a temporary object to add two objects
-    if(t1->getMyQueueSize() <= 0 || t2->getMyQueueSize() <=0 ){   //checking if queue is empty or not
+MYQUEUE<T> MakeCombinedQueue(MYQUEUE<T> *q1, MYQUEUE<T> *q2){
+    if(q1.getMyQueueValues().empty() || q2.getMyQueueValues().empty()){
         throw std::runtime_error("Queue is empty");
     }
-        int cap = t1->getMyQueueCapacity() + t2->getMyQueueCapacity();
-        t3->setMyQueueCapacity(cap); //adding both instance capacity to t3
-        int size =  t1->getMyQueueSize() + t2->getMyQueueSize();
-        t3->setMyQueueSize(size);
-    for(int i=0;i<t3->getMyQueueSize();i++){
-            //t3->setMyQueueValues() = t1->getMyQueueValues() + t2->getMyQueueValues();
+    std::list<T> data;
+    for(auto &val: q1){
+        data.push_back(val);
+        //data.size()++;
     }
-    return *t3;
+    for(auto &val: q2){
+        data.push_back(val);
+        //data.size()++;
+    }
+
+    MYQUEUE<T> *q3;
+    for(auto &it: data){
+        q3-> pushBack(it);
+    }
+    return *q3;
 }
+
+
 template<typename T>
-MYQUEUE<T> FindCommonQueueElements(MYQUEUE<T> *t1,MYQUEUE<T> *t2){
-    MYQUEUE<T> *t3;
-    if(t1->getMyQueueSize() <= 0 || t2->getMyQueueSize() <= 0) {  //checking if queue is empty or not
-            throw std::runtime_error("Queue is empty");
+MYQUEUE<T> FindCommonQueueElements(MYQUEUE<T> &q1,MYQUEUE<T> &q2){
+    if(q1.getMyQueueValues().empty() || q2.getMyQueueValues().empty()){
+        throw std::runtime_error("Queue is empty");
     }
-    for(int i=0;i< t1->getMyQueueCapacity() ; i++){
-        for(int j=0; j < t2->getMyQueueCapacity(); j++){
-            if(t1->getMyQueueValues() == t2->getMyQueueValues()){
-                t3->setMyQueueValues() = t1->getMyQueueValues();
-                t3->setMyQueueSize()++;
-            }
-        }
-    }
-    return *t3;
 }
