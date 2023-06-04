528ef28df39195d0192460f9c8da9ad00766b208
JazibEqbal <jazibeqbal0526@gmail.com>
2023-06-04 23:09:03 +0530

    Account Lambda

--- STL/AccountLambda/Account.h
+++ STL/AccountLambda/Account.h
@@ -11,11 +11,11 @@ private:
     float accountBalance;
     enum class ACCOUNT_TYPE accountType;
     std::string accountHolderName;
-    std::list<Transaction*> accountLast5Transactions;
+    Transaction* accountLast5Transactions;
 public:
     Account() = delete;
     Account(const Account &obj) = default;
-    Account(std::string id,float balance,enum class ACCOUNT_TYPE type,std::string name,std::list<Transaction*> trans);
+    Account(std::string id,float balance,enum class ACCOUNT_TYPE type,std::string name,Transaction* trans);
     ~Account();
 
     std::string getAccountId() const { return accountId; }
@@ -29,9 +29,9 @@ public:
 
     enum class ACCOUNT_TYPE getAccountType() const {return accountType;};
 
-    std::list<Transaction*> getAccountLast5Transactions() const { return accountLast5Transactions; }
-    void setAccountLast5Transactions(const std::list<Transaction*> &accountLast5Transactions_) { accountLast5Transactions = accountLast5Transactions_; }
-    
+    Transaction* getAccountLast5Transactions() const { return accountLast5Transactions; }
+    void setAccountLast5Transactions(Transaction* accountLast5Transactions_) { accountLast5Transactions = accountLast5Transactions_; }
+
     friend std::ostream &operator<<(std::ostream &os, const Account &rhs);
 };
 
