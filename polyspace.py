06c7cb6b270901501ed22fbd66ceebe7c7fd3610
Jazib Eqbal <79412764+JazibEqbal@users.noreply.github.com>
2023-08-03 17:23:45 +0530

    Update polyspace.py

--- polyspace.py
+++ polyspace.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, ttk, Button, Text, Label, Canvas
 from tkinter import *
-from PIL import ImageTk, Image
+# from PIL import ImageTk, Image
 
 
 class HTMLFileLoaderApp:
@@ -24,14 +24,15 @@ class HTMLFileLoaderApp:
         self.progress_bar = None
         self.output_log = None
         self.output_reset_log = None
+        self.output_failure = None
         self.create_widgets()
 
     def create_widgets(self):
-        img = Image.open("logo.png")
-        logo = ImageTk.PhotoImage(img)
+        # img = Image.open(r"C:\Users\jazibe\Desktop")
+        # logo = ImageTk.PhotoImage(img)
 
-        label1 = tk.Label(image=logo)
-        label1.img = logo
+        label1 = tk.Label(self.window, text='Image', bg='#ffb6c1')
+        # label1.img = logo
         label1.place(x=0, y=0, width=80, height=70)
 
         self.zip_button = tk.Button(self.window, text='CREATE ZIP', bg='#000', fg='#fff',
@@ -67,19 +68,22 @@ class HTMLFileLoaderApp:
 
         # progress bar
         self.progress_bar = ttk.Progressbar(self.window, variable=self.progress_var, maximum=100, mode='determinate')
-        self.progress_bar.place(x=0, y=101, width=650, height=25)
+        self.progress_bar.place(x=0, y=101, width=650, height=24)
 
+    # function to handle load html file
     def load_html_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("HTML files", "*.html")])
-        if file_path:
-            if not self.hold_file_path.get():
+        file_path = filedialog.askopenfilename(filetypes=[("HTML files", "*.html")])  # getting the file path in
+        # file_path variable and opening a file dialog to select an html file
+        if file_path:   # if file_path has got some file path then proceed
+            if not self.hold_file_path.get():   # if file_path is empty writing to it
                 self.hold_file_path.set(file_path)
             else:
-                self.hold_file_path.set(file_path)  # over riding/updating
-            self.text_html.delete(1.0, 'end')
-            self.text_html.insert('end', file_path)
+                self.hold_file_path.set(file_path)  # if hold_file_path has a data then over-riding it
+            self.text_html.delete(1.0, 'end')  # clearing the text_html entry
+            self.text_html.insert('end', file_path)  # appending file_path to it here
             # self.getPath()
 
+    # function to handle load xml file
     def load_excel_file(self):
         file_path = filedialog.askopenfilename(filetypes=[('XML files', '*.xml')])
         if file_path:
@@ -90,8 +94,9 @@ class HTMLFileLoaderApp:
             self.text_excel.delete(1.0, 'end')
             self.text_excel.insert('end', file_path)
 
+    # function to handle create zip file
     @staticmethod
-    def load_zip_file():
+    def load_zip_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Zip files", "*.zip")])
         # if file_path:
         #     if not self.hold_file_path.get():
@@ -103,8 +108,9 @@ class HTMLFileLoaderApp:
 
     # function to get the selected file path
     def get_path(self):
-        return self.hold_file_path.get()
+        return self.hold_file_path.get()  # returning the hold_file_path data
 
+    # execute function to handle execute button functions
     def execute(self):
         progress = 0  # declaring a variable to hold value of progress execution
         if len(self.hold_file_path.get()) > 0:  # on clicking execute button it checks if the file if selected or not
@@ -117,6 +123,9 @@ class HTMLFileLoaderApp:
                 if progress == 100:  # checking if progress is 100% done or not, if done then only showing success msg
                     self.set_execution(True)
                     self.success_msg()  # calling success function
+                # if progress == 60:  # just for demonstrating failure output
+                #     self.failure_msg()
+                #     break
 
         self.text_html.delete(1.0, 'end')  # deleting text inside html text area after execution
         self.text_excel.delete(1.0, 'end')  # deleting text inside excel text area after execution
@@ -124,28 +133,40 @@ class HTMLFileLoaderApp:
         self.execute_button.config(text="EXECUTE")  # Reset the button text
         self.hold_file_path.set('')  # setting the file path again to zero
 
+    # function to update the execution_success variable to true
     def set_execution(self, execution_success):
         self.execution_success = execution_success
         # print(self.execution_success)
-        # print('hehe')
-
+        # print('here')
+    
+    # function to show up when process executed 100%
     def success_msg(self):
         if self.execution_success:  # if execution was 100% then only
             # print('Before')
             log_report = f'File Parsed Successfully!. Log report can be found at {self.get_path()}'
             self.output_log = tk.Label(self.window, text=log_report, bg='green', anchor=W)
             # print('After')
-            self.output_log.place(x=0, y=127, width=650, height=25)
+            self.output_log.place(x=0, y=125, width=650, height=29)
             self.window.update_idletasks()
             self.window.after(3000)  # adding a delay of 3s so that can user can view the success msg
-            self.reset_success_msg()  # calling reset_success_msg() function
+            self.reset_msg()  # calling reset_msg() function
 
-    def reset_success_msg(self):
+    # function to show up when execution fails
+    def failure_msg(self):
+        self.output_failure = tk.Label(self.window, text='Failed! Watch logs for fixing it!.', bg='red', anchor=W)
+        self.output_failure.place(x=0, y=125, width=650, height=29)
+        self.window.update_idletasks()
+        self.window.after(3000)  # adding a delay of 3s so that can user can view the success msg
+        self.reset_msg()  # calling reset_msg() function
+    
+    # function to reset output log to empty
+    def reset_msg(self):
         # print('before')
         self.output_reset_log = Label(self.window, text='', anchor=W)
-        self.output_reset_log.place(x=0, y=127, width=650, height=25)
+        self.output_reset_log.place(x=0, y=125, width=650, height=29)
         # print('after')
-
+    
+    # function to close app, handled by exit button
     def exit_app(self):
         self.window.quit()  # calling quit method to handle click and close the window
 
